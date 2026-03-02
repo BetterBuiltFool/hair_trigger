6c42bbb12b21eb471f22dfc23f27b228b9fadae2
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-03-02 13:52:09 -0500

    Change DeferredScheduler to StackScheduler StackScheduler uses a stack rather than a dictionary. A dictionary was a poor choice. Renamed to allow for other types of deferred schedulers

--- src/hair_trigger/scheduler.py
+++ src/hair_trigger/scheduler.py
@@ -1,14 +1,13 @@
 from __future__ import annotations
 
 from typing import Any, TYPE_CHECKING
-from weakref import WeakKeyDictionary
 
 from .typing.scheduler import Scheduler
 
 if TYPE_CHECKING:
     from hair_trigger.event import Event
 
-    type EventArgs = tuple[Any, Any]
+    type EventArgs = tuple[Event, Any, Any]
 
 
 class InstantScheduler(Scheduler):
@@ -20,18 +19,16 @@ class InstantScheduler(Scheduler):
         event._notify(*args, **kwds)
 
 
-class DeferredScheduler(Scheduler):
+class StackScheduler(Scheduler):
     """
-    Scheduler that stores events and arguments until called upon.
+    Scheduler that stores events and arguments in a stack until called upon.
     """
 
     def __init__(self) -> None:
-        self._scheduled_events: WeakKeyDictionary[Event, EventArgs] = (
-            WeakKeyDictionary()
-        )
+        self._scheduled_events: list[EventArgs] = []
 
     def schedule(self, event: Event[Any], *args, **kwds) -> None:
-        self._scheduled_events[event] = (args, kwds)
+        self._scheduled_events.append((event, args, kwds))
 
 
 _active_scheduler: Scheduler = InstantScheduler()
