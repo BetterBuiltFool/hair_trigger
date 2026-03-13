3eaea6c3a6f1bd5284dd451e3318919635095dc9
BetterBuiltFool <betterbuiltfool@gmail.com>
2026-03-13 13:55:24 -0400

    Rename config module to configure Name clash might have been causing issues in the wheel

--- src/hair_trigger/__init__.py
+++ src/hair_trigger/__init__.py
@@ -1,3 +1,3 @@
-from .config import config as configure  # noqa:F401
+from .configure import config  # noqa:F401
 from .event import Event  # noqa:F401
 from .scheduler import pump_events  # noqa:F401
